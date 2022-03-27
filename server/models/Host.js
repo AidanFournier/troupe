703dc109037002e2f3c79416df067d7698eb2200
80andAbove <shogomiyagi94@gmail.com>
2022-03-28 01:23:53 +0900

    Implemented model Host

---
+++ server/models/Host.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const hostSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+    },
+    tripId: {
+        type: Schema.Types.ObjectId,
+        ref: 'trip',
+    }
+});
+
+const Host = mongoose.model('host', hostSchema);
+
+module.exports = Host;
