df58969845a87188506c2b51141b25d4f1df8768
80andAbove <shogomiyagi94@gmail.com>
2022-03-28 00:02:10 +0900

    Connection to MongoDB Atlas successful

--- server/index.js
+++ server/index.js
@@ -1,7 +1,29 @@
 // server/index.js
 const path = require("path");
 const express = require("express");
+require('dotenv').config();
 
+const { MongoClient } = require("mongodb");
+ 
+// Replace the following with your Atlas connection string
+const url = process.env.ATLAS_URI;
+const client = new MongoClient(url);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB Atlas");
+
+    } catch (err) {
+        console.log("Cant connect");
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -25,3 +47,4 @@ app.get("/*", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
