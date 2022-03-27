d99ee9c7a70856d45472402ec8bb3fd9eea1b661
80andAbove <shogomiyagi94@gmail.com>
2022-03-28 01:18:32 +0900

    Implemented model User

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+
+    },
+    img_url: {
+        type: String,
+        required: false,
+    },
+});
+
+var User = mongoose.model('user', userSchema);
+
+module.exports = User;
