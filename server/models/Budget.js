f6904530685c92468d12dcfbb03a69375d0bdb16
80andAbove <shogomiyagi94@gmail.com>
2022-03-28 01:38:12 +0900

    Implemented model Budget

---
+++ server/models/Budget.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const budgetSchema = new Schema({
+    tripId: {
+        type: Schema.Types.ObjectId,
+        ref: 'trip',
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+    },
+    category: {
+        type: Number,
+        required: false,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+});
+
+const Budget = mongoose.model('budget', budgetSchema);
+
+module.exports = Budget;
