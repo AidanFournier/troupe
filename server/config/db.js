4f1d2509e59d1e59a582f4ad809cd4a7b3cab4c2
80andAbove <shogomiyagi94@gmail.com>
2022-04-05 19:49:41 +0900

    Changed db from using MongoDB Client to mongoose

--- server/config/db.js
+++ server/config/db.js
@@ -1,21 +1,17 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
     const url = process.env.ATLAS_URI;
-    const client = new MongoClient(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    try {
-        await client.connect();
-        console.log('Connection to MongoDB successful');
-    } catch (error) {
-        console.log("Cannot connect to MongoDB");
-        console.log(error);
-    } finally{
-        await client.close();
-    }
+    mongoose
+        .connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then((data) => {
+            console.log(`MongoDB connected with server: ${data.connection.host}`)
+        });
+
 };
 
 module.exports = connectDB;
