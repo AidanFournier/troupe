b9a752840cb685b0680718e002899b7845629ac3
80andAbove <shogomiyagi94@gmail.com>
2022-04-05 19:45:56 +0900

    Changed findUser to loginUser

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', registerUser);
-router.get('/:id', findUser);
+router.get('/:id', loginUser);
 router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
+
+// still need to implement findUser, resetPassword, logout, forgotPassword
 
 module.exports = router;
